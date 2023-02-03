ff51939830aa334ae5cc2359c4eb68faa877eb21
Jay W <40270505+JayDoubleu@users.noreply.github.com>
2023-02-03 07:59:51 +0000

    Add dummy pipeline (#1)

---
+++ components/cnp-dummy-library-test/provider.tf
@@ -0,0 +1,34 @@
+terraform {
+  backend "azurerm" {}
+  required_version = "0.13.0"
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.33.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.41.0"
+    }
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = "0.1.0"
+    }
+    pkcs12 = {
+      source  = "chilicat/pkcs12"
+      version = "0.0.7"
+    }
+    panos = {
+      source  = "PaloAltoNetworks/panos"
+      version = "1.11.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "5.3.0"
+    }
+    dynatrace = {
+      version = "1.18.1"
+      source  = "dynatrace-oss/dynatrace"
+    }
+  }
+}
